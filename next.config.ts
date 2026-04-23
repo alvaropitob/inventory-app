99eb39b1d5f1d2e9e5ff61806f94b238469eb651
Alvaro Pito <alvaro_pito@hotmail.com>
2026-04-22 20:36:27 -0500

    debug: super clean build - removing all possible sources of error

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,7 @@
 import type { NextConfig } from "next";
-import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
-
-// export default withSentryConfig(nextConfig, {
-//   org: "alvaropito",
-//   project: "inventory-app",
-//   silent: !process.env.CI,
-//   widenClientFileUpload: true,
-//   reactComponentAnnotation: { enabled: true },
-//   tunnelRoute: "/monitoring",
-//   disableLogger: true,
-//   automaticVercelMonitors: true,
-// });
